8d596dfe17d640fa5825bb871daf7d8398f5acd0
Debasish Mandal <debasishm89@gmail.com>
2016-05-13 10:02:06 +0530

    Update QuickPageHeap.bat

--- QuickPageHeap.bat
+++ QuickPageHeap.bat
@@ -1,18 +1,24 @@
 @ECHO OFF
 ECHO PageHeap Enable / Diable Batch Script - Please run as admin
-set /p Choice=Want to Enable or Disable PageHeap? (e or d):%=%
-set /p procName=Enter Process Name(Ex. iexplorer.exe):%=%
+set /p Choice=Want to Enable, Disable or List PageHeap enabled programs? (e , d or l):%=%
+SET GFLAG_PATH="C:\Program Files\Debugging Tools for Windows (x86)\gflags.exe"
 
 if "%Choice%"=="e" goto :ENABLE
 if "%Choice%"=="d" goto :DISABLE
+if "%Choice%"=="l" goto :LISTPROGS
 
 :ENABLE
-	"C:\Program Files\Debugging Tools for Windows (x86)\gflags.exe" /p /enable %procName%  /full
-	"C:\Program Files\Debugging Tools for Windows (x86)\gflags.exe" /i %procName% +hpa
-	"C:\Program Files\Debugging Tools for Windows (x86)\gflags.exe" /i %procName% +02000000
+	set /p procName=Enter Process Name(Ex. iexplorer.exe):%=%
+	%GFLAG_PATH% /p /enable %procName%  /full
+	%GFLAG_PATH% /i %procName% +hpa
+	%GFLAG_PATH% /i %procName% +02000000
 	goto :EOF
 :DISABLE
-	"C:\Program Files\Debugging Tools for Windows (x86)\gflags.exe" /p /disable %procName%  /full
-	"C:\Program Files\Debugging Tools for Windows (x86)\gflags.exe" /i %procName% -hpa
-	"C:\Program Files\Debugging Tools for Windows (x86)\gflags.exe" /i %procName% -02000000
+	set /p procName=Enter Process Name(Ex. iexplorer.exe):%=%
+	%GFLAG_PATH% /p /disable %procName%  /full
+	%GFLAG_PATH% /i %procName% -hpa
+	%GFLAG_PATH% /i %procName% -02000000
+	goto :EOF
+:LISTPROGS
+	%GFLAG_PATH% /p
 	goto :EOF
