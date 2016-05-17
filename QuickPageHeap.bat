9c51fd6421fab4e89e2be8cec00c0dfd1d0d9d95
Debasish Mandal <debasishm89@gmail.com>
2016-05-17 11:45:59 +0530

    Update QuickPageHeap.bat

--- QuickPageHeap.bat
+++ QuickPageHeap.bat
@@ -1,20 +1,28 @@
 @ECHO OFF
-ECHO PageHeap Enable / Diable Batch Script - Please run as admin
-set /p Choice=Want to Enable, Disable or List PageHeap enabled programs? (e , d or l):%=%
+
 SET GFLAG_PATH="C:\Program Files\Debugging Tools for Windows (x86)\gflags.exe"
+
+IF EXIST %GFLAG_PATH% (
+	ECHO Full PageHeap Enable / Disable Batch Script - Please run as admin
+) ELSE (
+	ECHO Path of %GFLAG_PATH% doesnt exist. Please set correct path. 
+	goto :EOF
+)
+
+set /p Choice=Want to Enable,Disable or List PageHeap enabled programs? (e or d or l):%=%
 
 if "%Choice%"=="e" goto :ENABLE
 if "%Choice%"=="d" goto :DISABLE
 if "%Choice%"=="l" goto :LISTPROGS
 
 :ENABLE
-	set /p procName=Enter Process Name(Ex. iexplorer.exe):%=%
+	set /p procName=Enter Process Name(Ex. iexplore.exe):%=%
 	%GFLAG_PATH% /p /enable %procName%  /full
 	%GFLAG_PATH% /i %procName% +hpa
 	%GFLAG_PATH% /i %procName% +02000000
 	goto :EOF
 :DISABLE
-	set /p procName=Enter Process Name(Ex. iexplorer.exe):%=%
+	set /p procName=Enter Process Name(Ex. iexplore.exe):%=%
 	%GFLAG_PATH% /p /disable %procName%  /full
 	%GFLAG_PATH% /i %procName% -hpa
 	%GFLAG_PATH% /i %procName% -02000000
